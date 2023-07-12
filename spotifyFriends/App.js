a41f7b5e689f6e8eb587e7a55a90170bc668871f
dourian <dorcwork@gmail.com>
2023-07-12 01:12:42 -0400

    Co-authored-by: Jacqueline Ho <jho426@users.noreply.github.com>

--- spotifyFriends/App.js
+++ spotifyFriends/App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import * as WebBrowser from "expo-web-browser";
+import {
+  makeRedirectUri,
+  ResponseType,
+  useAuthRequest,
+} from "expo-auth-session";
+import { Button, Text } from "react-native";
+
+WebBrowser.maybeCompleteAuthSession();
+
+// Endpoint
+const discovery = {
+  authorizationEndpoint: "https://accounts.spotify.com/authorize",
+  tokenEndpoint: "https://accounts.spotify.com/api/token",
+};
 
 export default function App() {
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      responseType: ResponseType.Token,
+      clientId: "942c1e8214364434a9317b79856493dc",
+      scopes: ["user-read-email", "playlist-modify-public"],
+      // In order to follow the "Authorization Code Flow" to fetch token after authorizationEndpoint
+      // this must be set to false
+      usePKCE: false,
+      redirectUri: makeRedirectUri({
+        scheme: "spotifyFriends://",
+      }),
+    },
+    discovery
+  );
+
+  React.useEffect(() => {
+    if (response?.type === "success") {
+      const { access_token } = response.params;
+    }
+  }, [response]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Text>PLACE HOLDER</Text>
+      <Text>PLACE HOLDER</Text>
+      <Text>PLACE HOLDER</Text>
+      <Text>PLACE HOLDER</Text>
+      <Button
+        disabled={!request}
+        title="Login"
+        onPress={() => {
+          promptAsync();
+        }}
+      />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
