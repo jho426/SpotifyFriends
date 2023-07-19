b78c4b12bea8039f7665e0787d4e44467243b75a
dourian <dorcwork@gmail.com>
2023-07-19 01:37:42 -0400

    prod
    Co-authored-by: Jacqueline Ho <jacquelineho426@gmail.com>

--- spotifyFriends/tailwind.config.js
+++ spotifyFriends/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+        backgroundImage: {
+            'start-bg': "url('./src/assets/login.png')",
+        }
+    },
   },
   plugins: [],
 }
